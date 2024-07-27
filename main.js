7b0651f15b27298ef172a1c9ef7ceb203438b43c
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-07-27 16:08:21 -0500

    feat: :sparkles: Finished query #2 (setTicket)
    feat: :sparkles: fixed two tiny errors in the code of query #1 y #2 and organized all the functions in the main file

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 // import{testConnection} from './js/model/testConnection.js';
 // import{getAllMovies} from './js/model/peliculas.js';
 import{MovieService} from './js/model/peliculas.js';
-let obj = new MovieService
+import{ticketService} from './js/model/boletos.js';
+// const { MongoClient, ObjectId} = require('mongodb');
+import { ObjectId } from 'mongodb';
 // console.log(await testConnection())
 
-console.log(await obj.getAllMovies())
+//PELICULAS ---------------
+
+let peliculas = new MovieService 
+
+// QUERY #1 ----**Selección de Películas:**
+
+// console.log(await peliculas.getAllMovies())
+
+let obj2 = new ticketService
+
+console.log(await obj2.setTicket(2, 2, {
+    'fila': 'B',
+    'numero': 1,
+    'tipo': 'vip'
+}, 'efectivo' )) //proyeccion_id, usuario_id, asiento, metodo_pago

29f86a0fa9fa549eb58c326f0e1f7fc0fe564f46
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-08-06 12:35:03 -0500

    feat: :sparkles: added a file for implementing express

---
+++ app.js
@@ -0,0 +1,10 @@
+const express  = require("express")
+
+const app = express();
+
+const host = "localhost"
+const port = 3001
+
+app.get("/peliculas",(req, res),  ()=>{
+    let peliculasDisp = 
+} )

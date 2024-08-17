1afb4b8b508b2c879d32aefcaf776959e99992d1
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-08-17 08:56:52 -0500

    feat: :sparkles: added a funcitonality for playing the trailer when the button  for it is clicked

--- public/js/module/peliculas_info.js
+++ public/js/module/peliculas_info.js
@@ -1,65 +1,99 @@
+function getYouTubeVideoID(url) {
+    const urlObj = new URL(url);
+    let videoID;
+
+    if (urlObj.pathname.startsWith('/embed/')) {
+        // El ID del video es la parte después de "/embed/"
+        videoID = urlObj.pathname.split('/embed/')[1].split('?')[0];
+    } else {
+        // Maneja otros casos (como con "v=")
+        const urlParams = new URLSearchParams(urlObj.search);
+        videoID = urlParams.get('v');
+    }
+
+    return videoID;
+}
+
+// Ejemplo de uso
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const movieId = window.location.pathname.split('/').pop();
+
     fetch(`/api/movies/${movieId}`)
-      .then(response => response.json())
-      .then(movie => {
-        const movieDetailsContainer = document.getElementById('main');
-        movieDetailsContainer.innerHTML = `
-          <section id="video-main" class="video-main">
-            <img src="${movie.poster_url}" alt="${movie.titulo}">
+        .then(response => response.json())
+        .then(movie => {
+            const videoId = getYouTubeVideoID(movie.trailer);
+            const movieDetailsContainer = document.getElementById('main');
 
-          
+            movieDetailsContainer.innerHTML = `
+            <section class="video__container">
+                <div id="cover-container" class="cover-main">
+                    <img id="cover-image"  src=${movie.poster_url} alt=${movie.titulo}>
+                </div>
+                <lite-youtube id="video-main" class="video-main" videoid="${videoId}" style="display:none;"></lite-youtube>
+            </section>
 
+            <section class="info_video-main">
+                <div>
+                    <h5>${movie.titulo}</h5>
+                    <p>${movie.genero}</p>
+                </div>
+                <button id="watch-trailer">Watch Trailer</button>
+            </section>
 
-            
-        </section>
-        <section class="info_video-main">
-            <div>
-                <h5>${movie.titulo}</h5>
-                <p>${movie.genero}</p>
-            </div>
-            <button>Watch Trailer</button>
-        </section>
-        <section class="detalle_video-main">
-            <p class="p">${movie.sinopsis}</p>
-            <div class="detalle_video-actor">
-                <p>Cast</p>
-                <div class="actor-list">
-                    <div class="actor-info">
-                        <img src="${movie.actores[0].actor_img}" alt="${movie.actores[0].actor}">
-                        <div>
-                            <p class="actor-name">${movie.actores[0].actor}</p>
-                            <p class="actor-role">${movie.actores[0].personaje}</p>
+            <section class="detalle_video-main">
+                <p class="p">${movie.sinopsis}</p>
+                <div class="detalle_video-actor">
+                    <p>Cast</p>
+                    <div class="actor-list">
+                        <div class="actor-info">
+                            <img src="${movie.actores[0].actor_img}" alt="${movie.actores[0].actor}">
+                            <div>
+                                <p class="actor-name">${movie.actores[0].actor}</p>
+                                <p class="actor-role">${movie.actores[0].personaje}</p>
+                            </div>
                         </div>
-                    </div>
-                   <div class="actor-info">
-                        <img src="${movie.actores[1].actor_img}" alt="${movie.actores[1].actor}">
-                        <div>
-                            <p class="actor-name">${movie.actores[1].actor}</p>
-                            <p class="actor-role">${movie.actores[1].personaje}</p>
+                        <div class="actor-info">
+                            <img src="${movie.actores[1].actor_img}" alt="${movie.actores[1].actor}">
+                            <div>
+                                <p class="actor-name">${movie.actores[1].actor}</p>
+                                <p class="actor-role">${movie.actores[1].personaje}</p>
+                            </div>
                         </div>
-                    </div>
-                   <div class="actor-info">
-                        <img src="${movie.actores[2].actor_img}" alt="${movie.actores[2].actor}">
-                        <div>
-                            <p class="actor-name">${movie.actores[2].actor}</p>
-                            <p class="actor-role">${movie.actores[2].personaje}</p>
+                        <div class="actor-info">
+                            <img src="${movie.actores[2].actor_img}" alt="${movie.actores[2].actor}">
+                            <div>
+                                <p class="actor-name">${movie.actores[2].actor}</p>
+                                <p class="actor-role">${movie.actores[2].personaje}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-             <div class="detalle_video-cinema">
-                <p class="actor-name">Cinema</p>
-                <div class="cinema-info">
-                    <div>
-                        <p class="actor-name">Campuslands</p>
-                        <p class="actor-role">Zona Franca, Floridablanca</p>
+                <div class="detalle_video-cinema">
+                    <p class="actor-name">Cinema</p>
+                    <div class="cinema-info">
+                        <div>
+                            <p class="actor-name">Campuslands</p>
+                            <p class="actor-role">Zona Franca, Floridablanca</p>
+                        </div>
+                        <img src="https://i.postimg.cc/qRGy3vB2/furiosa.jpg" alt="Antonio Banderas">
                     </div>
-                    <img src="https://i.postimg.cc/qRGy3vB2/furiosa.jpg" alt="Antonio Banderas">
                 </div>
-            </div>
-        </section>
-        `;
-      })
-      .catch(error => console.error('Error:', error));
-  });
+            </section>
+            `;
+
+            // Event Listener para el botón "Watch Trailer"
+            const watchTrailerButton = document.getElementById('watch-trailer');
+            watchTrailerButton.addEventListener('click', () => {
+                const coverImage = document.getElementById('cover-image');
+                const video = document.getElementById('video-main');
+
+                // Ocultar la portada y mostrar el video
+                coverImage.style.display = 'none';
+                video.style.display = 'block';
+            });
+        })
+        .catch(error => console.error('Error:', error));
+});
